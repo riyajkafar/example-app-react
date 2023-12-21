2d830ba402da02f768be77dd64365ae6f8494e51
mrmrifad <rifadm32@gmail.com>
2023-12-21 23:46:55 +0530

    Authentications edited

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('brands.index')} active={route().current('brands.index')}>
+                                    Brands
+                                </NavLink>
                             </div>
                         </div>
 
