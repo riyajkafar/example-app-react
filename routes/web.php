2a0a8e4a8a1ece558072bb576d8c28051435af27
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-22 16:52:57 +0530

    feat: added the Product resource route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// Product Routes
+Route::resource('products', ProductController::class)
+    ->middleware('auth');
 
 require __DIR__.'/auth.php';
