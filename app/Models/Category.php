3ed564be524ee8d96583f56dd6200cfedd69f27e
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-22 16:20:54 +0530

    feat: added the code for HasMany function Product Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -16,4 +17,8 @@ class Category extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
